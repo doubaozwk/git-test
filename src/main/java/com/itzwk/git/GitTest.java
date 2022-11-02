3571662ca79d85fcb0a1e6bac2053ed713475535
Zwk <965733661@qq.com>
2022-11-02 18:43:25 +0800

    gitee commit

--- src/main/java/com/itzwk/git/GitTest.java
+++ src/main/java/com/itzwk/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("push");
         System.out.println("pull");
         System.out.println("test");
+        System.out.println("gitee");
     }
 }
